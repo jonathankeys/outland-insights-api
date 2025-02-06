a507be8e9f6b34e1a853a763499f0453ce167270
Jonathan Keys <jonkeys11@gmail.com>
2025-02-05 20:39:19 -0500

    [#1] Change project to Python Flask app

--- src/main/resources/db/migration/security/V1__create_user_password_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE user_passwords (
-	user_id VARCHAR(36) PRIMARY KEY,
-	password VARCHAR(255) NOT NULL,
-	created_at timestamp WITH time ZONE NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
-	updated_at timestamp WITH time ZONE NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC')
-);
