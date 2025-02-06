bac0efb0704a5d1e533b659c7154237147406b92
Jonathan Keys <jonkeys11@gmail.com>
2025-02-05 21:56:29 -0500

    [#3] Create database connection using SQL Alchemy
    - Create connection pool with SQL Alchemy
    - Create a shallow health check which just return 200
    - Create a deep health check which queries the database time
    - Wrap connection pool in a context manager to save state

--- README.md
+++ README.md
@@ -9,7 +9,12 @@ This is a simple Flask web application that returns "Hello, World!" when accesse
 docker compose up -d
 ```
 
+**Only Rebuild API**
+```
+docker compose up -d --no-deps --build api
+```
+
 **Send Test Request**
 ```
-curl http://localhost:7000
+curl http://localhost:5000/health/shallow
 ```
--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+import os
+from contextlib import contextmanager
+
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+PG_HOST = os.environ.get('PG_HOST', 'localhost')
+
+engine = create_engine(
+    f'postgresql://postgres:mysecretpassword@{PG_HOST}:5432/postgres',
+    pool_size=5,
+    max_overflow=10,
+    pool_timeout=30,
+    pool_recycle=1800,
+    pool_pre_ping=True
+)
+
+@contextmanager
+def get_connection():
+    connection = engine.connect()
+    try:
+        yield connection
+    finally:
+        connection.close()
+
+@app.route('/health/shallow')
+def health_shallow():
+    return jsonify({"message": "ok"}, 200)
+
+@app.route('/health/deep')
+def health_deep():
+    with get_connection() as conn:
+        try:
+            result = conn.execute(text("SELECT NOW();"))
+            return jsonify({"db_time": result.fetchone()[0]}, 200)
+        except Exception as e:
+            return jsonify({"error": str(e)}, 500)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
